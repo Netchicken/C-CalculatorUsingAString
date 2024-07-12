b1670f831ae88aa20772cdc5cef7ba9cf2a91c03
Gary Dix <intelproof@gmail.com>
2024-07-12 12:36:36 +1200

    formatting

--- CalculatorUsingAString/Form1.cs
+++ CalculatorUsingAString/Form1.cs
@@ -35,26 +35,24 @@ namespace CalculatorUsingAString
             lbxAnswer.Items.Add(Answer.getExpressionString() + " = " + Answer.calculate());
 
 
-            // Basic calculation
-            Expression e1 = new Expression("2+3-(3-4/2)+5!");
-            // Using built-in functions
-            Expression e2 = new Expression("sin(pi)+cos(pi)");
-            // Using fractions
-            Expression e3 = new Expression("1_2 + 1_1_2");
-            // Using various numeral systems
-            Expression e4 = new Expression("h.112f+o.123+b.00101+b7.1234");
-            // Using scientific notation
-            Expression e5 = new Expression("-1e10+1e-2+2+e+10");
-            // calculation
-            Console.WriteLine(e1.getExpressionString() + " = " + e1.calculate());
-            Console.WriteLine(e2.getExpressionString() + " = " + e2.calculate());
-            Console.WriteLine(e3.getExpressionString() + " = " + e3.calculate());
-            Console.WriteLine(e4.getExpressionString() + " = " + e4.calculate());
-            Console.WriteLine(e5.getExpressionString() + " = " + e5.calculate());
-
-
-
         }
 
     }
 }
+
+//// Basic calculation
+//Expression e1 = new Expression("2+3-(3-4/2)+5!");
+//// Using built-in functions
+//Expression e2 = new Expression("sin(pi)+cos(pi)");
+//// Using fractions
+//Expression e3 = new Expression("1_2 + 1_1_2");
+//// Using various numeral systems
+//Expression e4 = new Expression("h.112f+o.123+b.00101+b7.1234");
+//// Using scientific notation
+//Expression e5 = new Expression("-1e10+1e-2+2+e+10");
+//// calculation
+//Console.WriteLine(e1.getExpressionString() + " = " + e1.calculate());
+//Console.WriteLine(e2.getExpressionString() + " = " + e2.calculate());
+//Console.WriteLine(e3.getExpressionString() + " = " + e3.calculate());
+//Console.WriteLine(e4.getExpressionString() + " = " + e4.calculate());
+//Console.WriteLine(e5.getExpressionString() + " = " + e5.calculate());
